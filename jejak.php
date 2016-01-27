f43f79703431f3f221d02a22eb38ef2283dd058b
Kamil Alta <alternator99@gmail.com>
2016-01-27 11:28:13 +0800

    Hide error reporting.

--- jejak.php
+++ jejak.php
@@ -3,6 +3,8 @@
 	/*
 		A little code to trace client side information.
 	*/
+	error_reporting(0);
+	ini_set('display_errors', 0);
 	
 	$ip = @$_SERVER['REMOTE_ADDR'];
 	$compname = @gethostbyaddr($ip);
