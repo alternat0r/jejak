88588ebeb064a79fc500e95d63e7370c219b0bd8
Kamil Alta <alternator99@gmail.com>
2016-05-20 23:31:27 +0800

    Update jejak.php

--- jejak.php
+++ jejak.php
@@ -5,15 +5,18 @@
 	*/
 	error_reporting(0);
 	ini_set('display_errors', 0);
+	date_default_timezone_set('Asia/Kuala_Lumpur');
+
 	
 	$ip = @$_SERVER['REMOTE_ADDR'];
 	$compname = @gethostbyaddr($ip);
 	$useragent = @$_SERVER['HTTP_USER_AGENT'];
 	$referer = @$_SERVER['HTTP_REFERER'];
+	$today = date("m.d.y-H.i.s-"); 
 	
 	$msg = "IP: ".$ip."\nCOMPNAME: ".$compname."\nUSERAGENT: ".$useragent."\nREFERER: ".$referer."\n";
 
-	$myFile = "log.txt";
+	$myFile = "log_".$today.".txt";
 	$fh = @fopen($myFile, 'a');
 	$string = "IP: ".$ip."\r\nCOMPNAME: ".$compname."\r\nUSERAGENT: ".$useragent."\r\nREFERER: ".$referer."\r\n----\r\n";
 	fwrite($fh, $string);
